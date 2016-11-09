fba87c6a81fa1aa8f57ecd17a2ea673c03d14c40
Anti-k <romanslyzebniy@gmail.com>
2016-11-10 00:09:22 +0200

    views set

--- app/helpers/conversations_helper.rb
+++ app/helpers/conversations_helper.rb
@@ -1,2 +1,12 @@
 module ConversationsHelper
+
+  def get_addresser(conversation)
+    if conversation.sender_id == current_user.id || conversation.receiver_id == current_user.id
+      if conversation.sender_id == current_user.id 
+        receiver = User.find(conversation.receiver_id)
+      else
+        receiver = User.find(conversation.sender_id) 
+      end 
+    end
+  end
 end
